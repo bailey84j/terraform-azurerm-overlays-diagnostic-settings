6798f691bf31a6039835be13b7f4ae52a41d7841
John Spinella <jrspinella@users.noreply.github.com>
2024-04-22 11:40:07 -0400

    Update naming.tf

--- naming.tf
+++ naming.tf
@@ -5,7 +5,7 @@
 # Azure NoOps Naming - This should be used on all resource naming
 #------------------------------------------------------------
 data "azurenoopsutils_resource_name" "diagnostic_setting" {
-  name          = "default"
+  name          = var.workload_name
   resource_type = "azurerm_monitor_diagnostic_setting"
   prefixes      = [var.org_name, var.use_location_short_name ? module.mod_azure_region_lookup.location_short : var.location]
   suffixes      = compact([var.name_prefix == "" ? null : local.name_prefix, var.deploy_environment, local.name_suffix, var.use_naming ? "" : "diagsetting"])
